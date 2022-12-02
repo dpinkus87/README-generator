3c1ae27c0e9712fb56c0682a15908d32e9f75da8
dpinkus87 <dylanpinkus@gmail.com>
2022-12-02 08:32:59 -0600

    wrote function to write README file

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer")
+const fs = require("fs")
+const generateMarkdown = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    message: "Enter your name",
+    name: "name"
+},{
+    type: "input",
+    message: "Enter your age",
+    name: "age"
+},{
+    type: "list",
+    message: "Choose your favorite food",
+    name: "food",
+    choices: ["pizza", "ice cream", "cheesy bread"] 
+}];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    console.log(data);
+    fs.writeFile("./dist/README.md", generateMarkdown(data),err => console.log(err))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(answers => {
+        console.log(answers);
+ writeToFile("Dylan.md", answers)
+    })
+}
+
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,8 +12,10 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `Hello 
+  My name is ${data.name}
+  I am ${data.age} years old
+  My favorite food is ${data.food}
 `;
 }
 
